3b094af6200d865b3524f15cc5b0d05e1a50fa90
svalencia <svalencia@greenflex.com>
2016-11-06 12:22:14 +0100

    [Pagination] Update tests

--- src/Pagination/Pagination.test.js
+++ src/Pagination/Pagination.test.js
@@ -1,77 +1,75 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import { expect } from 'chai';
-import sinon from 'sinon';
 
 import Pagination from './Pagination';
 
-describe('<Pagination/>', function () {
+describe('<Pagination/>', () => {
   it('should render 5 pages', () => {
     const wrapper = mount(<Pagination totalPage={5} currentPage={1}></Pagination>);
 
-    expect(wrapper.find('li').length).to.equal(7);
-    expect(wrapper.find('.active').text()).to.equal('1');
-    expect(wrapper.find('li').at(0).hasClass('disabled')).to.be.true;
+    expect(wrapper.find('li').length).toEqual(7);
+    expect(wrapper.find('.is-active').text()).toEqual('1');
+    expect(wrapper.find('li').at(0).hasClass('disabled')).toBe(true);
   });
 
   it('should change to next page', () => {
-    const handleChange = sinon.spy()
+    const handleChange = jest.fn();
 
     const wrapper = mount(<Pagination totalPage={5} currentPage={1} onChange={(page) => {
-      expect(page).to.equal(2);
+      expect(page).toEqual(2);
       handleChange();
     }}></Pagination>);
 
     wrapper.find('li').at(6).simulate('click');
-    expect(handleChange.calledOnce).to.be.true;
+    expect(handleChange).toHaveBeenCalled();
   });
 
   it('should change to prev page', () => {
-    const handleChange = sinon.spy()
+    const handleChange = jest.fn();
 
     const wrapper = mount(<Pagination totalPage={5} currentPage={3} onChange={(page) => {
-      expect(page).to.equal(2);
+      expect(page).toEqual(2);
       handleChange();
     }}></Pagination>);
 
     wrapper.find('li').at(0).simulate('click');
-    expect(handleChange.calledOnce).to.be.true;
+    expect(handleChange).toHaveBeenCalled();
   });
 
   it('should change to a page', () => {
-    const handleChange = sinon.spy()
+    const handleChange = jest.fn()
 
     const wrapper = mount(<Pagination totalPage={5} currentPage={1} onChange={(page) => {
-      expect(page).to.equal(2);
+      expect(page).toEqual(2);
       handleChange();
     }}></Pagination>);
 
     wrapper.find('li').at(2).simulate('click');
-    expect(handleChange.calledOnce).to.be.true;
+    expect(handleChange).toHaveBeenCalled();
   });
 
   it('should change render 10 pages with 2 fillers', () => {
     const wrapper = mount(<Pagination totalPage={10} currentPage={5}></Pagination>);
 
-    expect(wrapper.find('.filler').length).to.equal(2);
-    expect(wrapper.find('.active').text()).to.equal('5');
-    expect(wrapper.text()).to.equal('chevron_left12...456...910chevron_right');
+    expect(wrapper.find('.filler').length).toEqual(2);
+    expect(wrapper.find('.is-active').text()).toEqual('5');
+    expect(wrapper.text()).toEqual('chevron_left12...456...910chevron_right');
   });
 
   it('should change render 10 pages with 1 right filler', () => {
     const wrapper = mount(<Pagination totalPage={10} currentPage={2}></Pagination>);
 
-    expect(wrapper.find('.filler').length).to.equal(1);
-    expect(wrapper.find('.active').text()).to.equal('2');
-    expect(wrapper.text()).to.equal('chevron_left12345...910chevron_right');
+    expect(wrapper.find('.filler').length).toEqual(1);
+    expect(wrapper.find('.is-active').text()).toEqual('2');
+    expect(wrapper.text()).toEqual('chevron_left12345...910chevron_right');
   });
 
   it('should change render 10 pages with 1 left filler', () => {
       const wrapper = mount(<Pagination totalPage={10} currentPage={10}></Pagination>);
 
-      expect(wrapper.find('.filler').length).to.equal(1);
-      expect(wrapper.find('.active').text()).to.equal('10');
-      expect(wrapper.text()).to.equal('chevron_left12...678910chevron_right');
-      expect(wrapper.find('li').at(9).hasClass('disabled')).to.be.true
+      expect(wrapper.find('.filler').length).toEqual(1);
+      expect(wrapper.find('.is-active').text()).toEqual('10');
+      expect(wrapper.text()).toEqual('chevron_left12...678910chevron_right');
+      expect(wrapper.find('li').at(9).hasClass('disabled')).toBe(true);
     });
 });
