e313073b6804825a15437d76a42a2afc280c3f83
svalencia <svalencia@greenflex.com>
2016-11-06 11:33:20 +0100

    [CircularPreloader] Update tests

--- src/CircularPreloader/CircularPreloader.test.js
+++ src/CircularPreloader/CircularPreloader.test.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { expect } from 'chai';
-import sinon from 'sinon';
 
 import CircularPreloader from './CircularPreloader';
 
 describe('<CircularPreloader/>', function () {
   it('should render a circular preloader', () => {
     const wrapper = shallow(<CircularPreloader />);
-    expect(wrapper.find('.preloader-wrapper').length).to.equal(1);
+    expect(wrapper.find('.mdl-spinner').length).toEqual(1);
   });
 });
