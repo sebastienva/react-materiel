b3a24149037ffcde5117dcb15a3331f2e7810bc4
svalencia <svalencia@greenflex.com>
2016-11-06 12:22:26 +0100

    [Modal] Update tests

--- src/Modal/Modal.test.js
+++ src/Modal/Modal.test.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import { expect } from 'chai';
 
 import Modal from './Modal';
 
-describe('<Modal/>', function () {
+describe('<Modal/>', () => {
   it('should render a modal', () => {
     const wrapper = mount(<Modal open={true}>Test</Modal>);
-    expect(wrapper.find('.modal').length).to.equal(1);
+    expect(wrapper.find('.mdl-dialog').length).toEqual(1);
 
-    wrapper.unmount();
   });
 });
