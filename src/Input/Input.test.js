7ca6138536e141337f94dd3d68cf297325788c59
svalencia <svalencia@greenflex.com>
2016-11-06 11:33:32 +0100

    [Input] Jest migration

--- src/Input/Input.test.js
+++ src/Input/Input.test.js
@@ -1,38 +1,36 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
-import { expect } from 'chai';
-import sinon from 'sinon';
+import { shallow } from 'enzyme';
 
 import Input from './Input';
 
-describe('<Input/>', function () {
+describe('<Input/>', () => {
   it('should have a floating label', () => {
     const wrapper = shallow(<Input label="bla" float value="" />);
 
     // check labelt
-    expect(wrapper.find('label').text()).to.equal('bla');
+    expect(wrapper.find('label').text()).toEqual('bla');
 
     // check "floating" on focus
     wrapper.find('input').simulate('focus');
-    expect(wrapper.find('.mdl-textfield').hasClass('is-focused')).to.be.true;
+    expect(wrapper.find('.mdl-textfield').hasClass('is-focused')).toBe(true);
 
     // still floating on blur when the value is not empty
     wrapper.setProps({ value: 'test' });
     wrapper.find('input').simulate('blur');
-    expect(wrapper.find('.mdl-textfield').hasClass('is-dirty')).to.be.true;
+    expect(wrapper.find('.mdl-textfield').hasClass('is-dirty')).toBe(true);
 
     // stop floating on blur when the value is empty
     wrapper.find('input').simulate('focus');
     wrapper.setProps({value: ''});
     wrapper.find('input').simulate('blur');
-    expect(wrapper.find('.mdl-textfield').hasClass('is-focused')).to.be.false;
+    expect(wrapper.find('.mdl-textfield').hasClass('is-focused')).toBe(false);
   });
 
   it('should have a placeholder', () => {
     const wrapper = shallow(<Input label="bla" value="" />);
 
-    expect(wrapper.find('label').text()).to.equal('bla');
-    expect(wrapper.find('.mdl-textfield').hasClass('mdl-textfield--floating-label')).to.be.false;
+    expect(wrapper.find('label').text()).toEqual('bla');
+    expect(wrapper.find('.mdl-textfield').hasClass('mdl-textfield--floating-label')).toBe(false);
   });
 
   it('should return value on onChange', (done) => {
@@ -41,7 +39,7 @@ describe('<Input/>', function () {
         label="change"
         value=""
         onChange={(value) => {
-          expect(value).to.equal('test');
+          expect(value).toEqual('test');
           done();
         }}
       />
@@ -51,29 +49,29 @@ describe('<Input/>', function () {
   });
 
   it('should call blur and focus callback', () => {
-    const handleBlur = sinon.spy();
-    const handleFocus = sinon.spy();
+    const handleBlur = jest.fn();
+    const handleFocus = jest.fn();
 
     const wrapper = shallow(
       <Input label="test" value="" onFocus={handleFocus} onBlur={handleBlur} />
     );
 
     wrapper.find('input').simulate('focus');
-    expect(handleFocus.calledOnce).to.be.true;
+    expect(handleFocus).toHaveBeenCalled();
 
     wrapper.find('input').simulate('blur');
-    expect(handleBlur.calledOnce).to.be.true;
+    expect(handleBlur).toHaveBeenCalled();
   });
 
   it('should have a character counter', () => {
     const wrapper = shallow(<Input label="bla" length={10} value="test" />);
 
-    expect(wrapper.find('.mdl-textfield__counter').text()).to.equal('4 / 10');
+    expect(wrapper.find('.mdl-textfield__counter').text()).toEqual('4 / 10');
   });
 
   it('should pass down undefined props', () => {
     const wrapper = shallow(<Input label="bla" maxLength="10" value="test" />);
 
-    expect(wrapper.find('input').prop('maxLength')).to.equal('10');
+    expect(wrapper.find('input').prop('maxLength')).toEqual('10');
   });
 });
